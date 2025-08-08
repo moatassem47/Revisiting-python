d85edc4cdfa03e7f7211cd2ba4a34d6fb3e7185d
moatassem47 <moatassem.320240182@ejust.edu.eg>
2025-08-08 16:33:47 +0000

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,22 @@
+# from array import array
+# scores = array('b') #array can only be in numeric data types
+# scores.append(11)
+# scores.append(12)
+# print(scores)
+# mylist = ["strings", 505, 'd', True] #list can take any type of data
+# print(mylist)
+
+# names = ["moatassem","bruce", "alphred"]
+# print(len(names))
+# names.insert(1,"clark")
+# print(names)
+# # names.sort()
+# # print(names)
+
+# heros = names[1:3]
+# print(heros)
+
+##dictionaries
+persons = {"batman": "bruce wayne", "superman":"clark kent", "batman's butler":"alphred bulter"}
+persons["the flash"] = "barry allen"
+print(persons["the flash"])
